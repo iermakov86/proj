7f44ef51b6e7f58ccc6d95c0a638517062204e85
Ermakov Igor <IVErmakov@inno.local>
2024-11-15 12:55:49 +0300

    Улучшили логику main и doSomething методов

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -4,13 +4,13 @@ public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("111111");
-        System.out.println("11--11");
+        System.out.println("11--1-1");
     }
 
     public static void doSomething(){
         System.out.println(1);
-        System.out.println(0);
-        System.out.println(0);
+        System.out.println(00);
+        System.out.println(00);
 
     }
 
