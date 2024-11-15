a52a349b760ff0555d0d08fae97dde43ca35640d
Ermakov Igor <IVErmakov@inno.local>
2024-11-15 12:40:20 +0300

    Изменена основная логика и добавлена коробка

---
+++ src/main/java/com/stepup/proj/Box.java
@@ -0,0 +1,9 @@
+package com.stepup.proj;
+
+public class Box {
+    private int size;
+
+    public int getSize() {
+        return size;
+    }
+}
--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -4,6 +4,11 @@ public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("111111");
+    }
+
+    public static void doSomething(){
+        System.out.println(1);
 
     }
+
 }
